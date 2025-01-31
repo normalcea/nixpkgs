7226d031e2d8a299794950942744db8376f46582
normalc <normalc@posteo.net>
2025-01-31 09:41:34 -0500

    parabolic: 2024.5.0 -> 2025.1.3

--- pkgs/by-name/pa/parabolic/package.nix
+++ pkgs/by-name/pa/parabolic/package.nix
@@ -1,77 +1,125 @@
-{ lib
-, buildDotnetModule
-, fetchFromGitHub
-, dotnetCorePackages
-, gtk4
-, libadwaita
-, pkg-config
-, wrapGAppsHook4
-, glib
-, shared-mime-info
-, gdk-pixbuf
-, blueprint-compiler
-, python3
-, ffmpeg
+{
+  lib,
+  stdenv,
+  fetchFromGitHub,
+  cmake,
+  gettext,
+  itstool,
+  ninja,
+  yelp-tools,
+  pkg-config,
+  libnick,
+  boost,
+  glib,
+  shared-mime-info,
+  gtk4,
+  libadwaita,
+  wrapGAppsHook4,
+  libxmlxx5,
+  blueprint-compiler,
+  qt6,
+  yt-dlp,
+  ffmpeg,
+  aria2,
+  nix-update-script,
+  useQtInterface ? false,
 }:
 
-buildDotnetModule rec {
+stdenv.mkDerivation (finalAttrs: {
   pname = "parabolic";
-  version = "2024.5.0";
+  version = "2025.1.3";
 
   src = fetchFromGitHub {
     owner = "NickvisionApps";
     repo = "Parabolic";
-    rev = version;
-    hash = "sha256-awbCn7W7RUSuEByXxLGrsmYjmxCrwywhhrMJq/iM1Uc=";
-    fetchSubmodules = true;
+    tag = "${finalAttrs.version}";
+    hash = "sha256-EzV3jjodS5E8BPmv982FlONrHqAnpbSFOtx5Gqobo44=";
   };
 
-  dotnet-sdk = dotnetCorePackages.sdk_8_0;
-  dotnet-runtime = dotnetCorePackages.runtime_8_0;
-  pythonEnv = python3.withPackages(ps: with ps; [ yt-dlp ]);
+  nativeBuildInputs =
+    [
+      cmake
+      gettext
+      ninja
+      pkg-config
+    ]
+    ++ (
+      if !useQtInterface then
+        [
+          itstool
+          yelp-tools
+          wrapGAppsHook4
+          blueprint-compiler
+          glib
+          shared-mime-info
+        ]
+      else
+        [ qt6.wrapQtAppsHook ]
+    );
 
-  projectFile = "NickvisionTubeConverter.GNOME/NickvisionTubeConverter.GNOME.csproj";
-  nugetDeps = ./deps.json;
-  executables = "NickvisionTubeConverter.GNOME";
+  buildInputs =
+    [
+      libnick
+      boost
+    ]
+    ++ (
+      if !useQtInterface then
+        [
+          glib
+          gtk4
+          libadwaita
+          libxmlxx5
+        ]
+      else
+        [
+          qt6.qtbase
+          qt6.qtsvg
+        ]
+    );
 
-   nativeBuildInputs = [
-    pkg-config
-    wrapGAppsHook4
-    glib
-    shared-mime-info
-    gdk-pixbuf
-    blueprint-compiler
+  cmakeFlags = [
+    (lib.cmakeFeature "UI_PLATFORM" (if !useQtInterface then "gnome" else "qt"))
   ];
 
-  buildInputs = [ gtk4 libadwaita ];
+  preFixup =
+    (if !useQtInterface then "gappsWrapperArgs" else "qtWrapperArgs")
+    + ''
+      +=(
+            --prefix PATH : ${
+              lib.makeBinPath [
+                aria2
+                ffmpeg
+                yt-dlp
+              ]
+            }
+          )
+    '';
 
-  runtimeDeps = [
-    gtk4
-    libadwaita
-    glib
-    gdk-pixbuf
-  ];
+  passthru.updateScript = nix-update-script { };
 
-  postPatch = ''
-    substituteInPlace NickvisionTubeConverter.Shared/Linux/org.nickvision.tubeconverter.desktop.in --replace '@EXEC@' "NickvisionTubeConverter.GNOME"
-  '';
+  meta = {
+    description = "Graphical frontend for yt-dlp to download video and audio";
+    longDescription = ''
+      Parabolic is a user-friendly frontend for `yt-dlp` that supports
+      many features including but limited to:
+      - Downloading and converting videos and audio using ffmpeg.
+      - Supporting multiple codecs.
+      - Offering YouTube sponsorblock support.
+      - Running multiple downloads at a time.
+      - Downloading metadata and video subtitles.
+      - Allowing the use of `aria2` for parallel downloads.
+      - Offering a graphical keyring to manage account credentials.
+      - Being available as both a Qt and GNOME application.
 
-  postInstall = ''
-    install -Dm444 NickvisionTubeConverter.Shared/Resources/org.nickvision.tubeconverter.svg -t $out/share/icons/hicolor/scalable/apps/
-    install -Dm444 NickvisionTubeConverter.Shared/Resources/org.nickvision.tubeconverter-symbolic.svg -t $out/share/icons/hicolor/symbolic/apps/
-    install -Dm444 NickvisionTubeConverter.Shared/Linux/org.nickvision.tubeconverter.desktop.in -T $out/share/applications/org.nickvision.tubeconverter.desktop
-  '';
-
-  makeWrapperArgs = [ "--prefix PATH : ${lib.makeBinPath [ pythonEnv ffmpeg ]}" ];
-
-  passthru.updateScript = ./update.sh;
-
-  meta = with lib; {
-    description = "Download web video and audio";
+      By default, the GNOME interface is used, but the Qt interface can
+      be built by overriding the `useQtInterface` parameter to `true`.
+    '';
     homepage = "https://github.com/NickvisionApps/Parabolic";
-    license = licenses.mit;
-    maintainers = with maintainers; [ ewuuwe ];
-    mainProgram = "NickvisionTubeConverter.GNOME";
-    platforms = platforms.linux;
+    license = lib.licenses.gpl3Plus;
+    maintainers = [
+      lib.maintainers.normalcea
+    ];
+    mainProgram = "org.nickvision.tubeconverter";
+    platforms = lib.platforms.linux;
   };
-}
+})
